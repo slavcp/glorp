34dab3b257e5169457d32af99bc21a2075a0a66b
deserialization <73188579+d15n@users.noreply.github.com>
2025-05-10 15:52:01 +0100

    fix undefined in settings when searching + format

--- src/frontend/settings.js
+++ src/frontend/settings.js
@@ -1,187 +1,153 @@
 import cSettings from "../cSettings.json";
 
 window.glorpClient.settings.changeSetting = (id, value, fromSlider) => {
-  if (document.querySelector(`#input_${id}`) && fromSlider) {
-    document.querySelector(`#input_${id}`).value = value;
-  } else document.querySelector(`#${id}`).value = value;
+	if (document.querySelector(`#input_${id}`) && fromSlider) {
+		document.querySelector(`#input_${id}`).value = value;
+	} else document.querySelector(`#${id}`).value = value;
 
-  switch (id) {
-    case "exitButton":
-      document.querySelector("#clientExit").style.display = `${
-        value ? "flex" : "none"
-      }`;
-      break;
-    case "menuTimer":
-      if (value) {
-        import("./components/menuTimer.css").then((css) => {
-          const menuTimerCSS = document.createElement("style");
-          menuTimerCSS.id = "menuTimerCSS";
-          menuTimerCSS.innerHTML = css.default;
-          document.head.appendChild(menuTimerCSS);
-        });
-      } else {
-        document.querySelector("#menuTimerCSS")?.remove();
-      }
-      break;
-    case "cleanUI": {
-      if (value) {
-        import("./components/clean.css").then((css) => {
-          const cleanCSS = document.createElement("style");
-          cleanCSS.id = "cleanCSS";
-          cleanCSS.innerHTML = css.default;
-          document.head.appendChild(cleanCSS);
-        });
-      } else {
-        document.querySelector("#cleanCSS")?.remove();
-      }
-      break;
-    }
-    case "textSelect": {
-      if (value) {
-        const textSelectCSS = document.createElement("style");
-        textSelectCSS.id = "textSelect";
-        textSelectCSS.innerHTML = "#chatHolder * { user-select: text }";
-        document.head.appendChild(textSelectCSS);
-      } else {
-        document.querySelector("#textSelect")?.remove();
-      }
-      break;
-    }
-  }
+	switch (id) {
+		case "exitButton":
+			document.querySelector("#clientExit").style.display = `${value ? "flex" : "none"}`;
+			break;
+		case "menuTimer":
+			if (value) {
+				import("./components/menuTimer.css").then((css) => {
+					const menuTimerCSS = document.createElement("style");
+					menuTimerCSS.id = "menuTimerCSS";
+					menuTimerCSS.innerHTML = css.default;
+					document.head.appendChild(menuTimerCSS);
+				});
+			} else {
+				document.querySelector("#menuTimerCSS")?.remove();
+			}
+			break;
+		case "cleanUI": {
+			if (value) {
+				import("./components/clean.css").then((css) => {
+					const cleanCSS = document.createElement("style");
+					cleanCSS.id = "cleanCSS";
+					cleanCSS.innerHTML = css.default;
+					document.head.appendChild(cleanCSS);
+				});
+			} else {
+				document.querySelector("#cleanCSS")?.remove();
+			}
+			break;
+		}
+		case "textSelect": {
+			if (value) {
+				const textSelectCSS = document.createElement("style");
+				textSelectCSS.id = "textSelect";
+				textSelectCSS.innerHTML = "#chatHolder * { user-select: text }";
+				document.head.appendChild(textSelectCSS);
+			} else {
+				document.querySelector("#textSelect")?.remove();
+			}
+			break;
+		}
+	}
 
-  const toggleFunctionName = `toggle${
-    id.charAt(0).toUpperCase() + id.slice(1)
-  }`;
-  if (typeof window.glorpClient.settings[toggleFunctionName] !== "function") {
-    try {
-      import(`./modules/${id}.js`);
-    } catch {
-      /*  */
-    }
-  } else {
-    window.glorpClient.settings[toggleFunctionName](value);
-  }
+	const toggleFunctionName = `toggle${id.charAt(0).toUpperCase() + id.slice(1)}`;
+	if (typeof window.glorpClient.settings[toggleFunctionName] !== "function") {
+		try {
+			import(`./modules/${id}.js`);
+		} catch {
+			/*  */
+		}
+	} else {
+		window.glorpClient.settings[toggleFunctionName](value);
+	}
 
-  window.glorpClient.settings.config[id] = value;
-  window.chrome.webview.postMessage(`setConfig,${id},${value}`);
+	window.glorpClient.settings.config[id] = value;
+	window.chrome.webview.postMessage(`setConfig,${id},${value}`);
 };
 
 class SettingsManager {
-  constructor() {
-    this.settingsWindow = window.windows[0];
-    this.init();
-  }
-  init() {
-    const origGetSettings = this.settingsWindow.getSettings;
-    this.settingsWindow.getSettings = (...args) =>
-      origGetSettings
-        .call(this.settingsWindow, ...args)
-        .replace(/^<\/div>/, "") + this.getCSettings();
+	constructor() {
+		this.settingsWindow = window.windows[0];
+		this.init();
+	}
+	init() {
+		const origGetSettings = this.settingsWindow.getSettings;
+		this.settingsWindow.getSettings = (...args) =>
+			origGetSettings.call(this.settingsWindow, ...args).replace(/^<\/div>/, "") + this.getCSettings();
 
-    this.settingsWindow.getCSettings = () => this.getCSettings();
-  }
+		this.settingsWindow.getCSettings = () => this.getCSettings();
+	}
 
-  searchMatches(setting) {
-    const query = this.settingsWindow.settingSearch.toLowerCase() || "";
-    return (
-      (setting.name.toLowerCase() || "").includes(query) ||
-      (setting.category.toLowerCase() || "").includes(query)
-    );
-  }
+	searchMatches(setting) {
+		const query = this.settingsWindow.settingSearch.toLowerCase() || "";
+		return (setting.name.toLowerCase() || "").includes(query) || (setting.category.toLowerCase() || "").includes(query);
+	}
 
-  generateHtml(option) {
-    const value = window.glorpClient.settings.config[option.id];
-    switch (option.type) {
-      case "checkbox":
-        return `<label class='switch'>
+	generateHtml(option) {
+		const value = window.glorpClient.settings.config[option.id];
+		switch (option.type) {
+			// biome-ignore format: it looks like straight ass when biome formats it
+			case "checkbox":
+				return `<label class='switch'>
                     <input id="${option.id}" type='checkbox'
-                           onclick='window.glorpClient.settings.changeSetting("${
-                             option.id
-                           }", this.checked, false)'
+                           onclick='window.glorpClient.settings.changeSetting("${option.id}", this.checked, false)'
                            ${value ? "checked" : ""}>
                     <span class='slider'></span>
                 </label>
-                ${
-                  option.button
-                    ? `<div class="settingsBtn" style="margin-right: 20px; width: auto" onclick="${option.buttonAction}">${option.button}</div>`
-                    : ""
-                }`;
-      case "slider":
-        return `<input type="number" class="sliderVal" id="input_${
-          option.id
-        }" min="1" max="5" value="${value || 1}" 
-                step="${
-                  option.step
-                }" oninput='window.glorpClient.settings.changeSetting("${
-          option.id
-        }", this.value, false)' style="margin-right:0px;border-width:0px">
-                <div class="slidecontainer" style="margin-top: -8px;"><input type="range" id="${
-                  option.id
-                }" min="${option.min}" max="${option.max}" 
-          step="${
-            option.step
-          }" value="${value}" class="sliderM" oninput='window.glorpClient.settings.changeSetting("${
-          option.id
-        }", this.value, true)'></div>`;
-      case "select":
-        return `<select id="${
-          option.id
-        }" class="inputGrey2" onchange='window.glorpClient.settings.changeSetting("${
-          option.id
-        }", this.value, false)'>
+                ${option.button
+						? `<div class="settingsBtn" style="margin-right: 20px; width: auto" onclick="${option.buttonAction}">${option.button}</div>`
+						: ""
+					}`;
+			// biome-ignore format: see above
+			case "slider":
+				return `<input type="number" class="sliderVal" id="input_${option.id}" min="1" max="5" value="${value || 1}" 
+                step="${option.step}" oninput='window.glorpClient.settings.changeSetting("${option.id}", this.value, false)' style="margin-right:0px;border-width:0px">
+                <div class="slidecontainer" style="margin-top: -8px;"><input type="range" id="${option.id}" min="${option.min}" max="${option.max}" 
+				step="${option.step}" value="${value}" class="sliderM" oninput='window.glorpClient.settings.changeSetting("${option.id}", this.value, true)'></div>`;
+			// biome-ignore format: see above
+			case "select":
+				return `<select id="${option.id}" class="inputGrey2" onchange='window.glorpClient.settings.changeSetting("${option.id}", this.value, false)'>
                     ${option.options.map(
-                      (opt) =>
-                        `<option value="${opt}" ${
-                          opt === value ? "selected" : ""
-                        }>${opt}</option>`
-                    )}
+					(opt) => `<option value="${opt}" ${opt === value ? "selected" : ""}>${opt}</option>`,
+				)}
                     </select>`;
-      case "none":
-        return "";
-    }
-  }
+			case "none":
+				return option.button
+					? `<div class="settingsBtn" style="margin-right: 20px; width: auto" onclick="${option.buttonAction}">${option.button}</div>`
+					: "";
+		}
+	}
 
-  getCSettings() {
-    if (
-      this.settingsWindow.tabs.advanced.length !==
-        this.settingsWindow.tabIndex + 1 &&
-      !this.settingsWindow.settingSearch
-    )
-      return "";
+	getCSettings() {
+		if (
+			this.settingsWindow.tabs.advanced.length !== this.settingsWindow.tabIndex + 1 &&
+			!this.settingsWindow.settingSearch
+		)
+			return "";
 
-    let tempHTML = "<div class='glorpSettings'>";
-    let previousCategory = null;
+		let tempHTML = "<div class='glorpSettings'>";
+		let previousCategory = null;
 
-    for (const entry of Object.keys(cSettings)) {
-      const setting = cSettings[entry];
-      setting.html = this.generateHtml(setting);
+		for (const entry of Object.keys(cSettings)) {
+			const setting = cSettings[entry];
+			setting.html = this.generateHtml(setting);
 
-      if (this.settingsWindow.settingSearch && !this.searchMatches(setting))
-        return;
+			if (this.settingsWindow.settingSearch && !this.searchMatches(setting)) continue;
 
-      if (previousCategory !== setting.category) {
-        if (previousCategory) tempHTML += "</div>";
+			if (previousCategory !== setting.category) {
+				if (previousCategory) tempHTML += "</div>";
 
-        previousCategory = setting.category;
-        tempHTML += `<div class='setHed' id="setHed_glorpClient_${setting.category}" onclick='window.windows[0].collapseFolder(this)'>
-                    <span class='material-icons plusOrMinus'>keyboard_arrow_down</span>${setting.category}</div>
+				previousCategory = setting.category;
+				tempHTML += `<div class='setHed' id="setHed_glorpClient_${setting.category}" onclick='window.windows[0].collapseFolder(this)'>
+                    <span class='material-icons plusOrMinus'>keyboard_arrow_down</span> ${setting.category}</div>
                     <div class='setBodH' id="setBod_glorpClient_${setting.category}">`;
-      }
+			}
 
-      tempHTML += `<div class='settName' ${
-        setting.description ? `title="${setting.description}"` : ""
-      }>
+			tempHTML += `<div class='settName' ${setting.description ? `title="${setting.description}"` : ""}>
                 ${setting.name}
-                ${
-                  setting.needsRestart
-                    ? ' <span style="color: #eb5656" title="Requires Restart">*</span>'
-                    : ""
-                } 
+                ${setting.needsRestart ? ' <span style="color: #eb5656" title="Requires Restart">*</span>' : ""} 
                 ${setting.html}</div>`;
-    }
+		}
 
-    return tempHTML ? `${tempHTML}</div></div></div>` : "";
-  }
+		return tempHTML ? `${tempHTML}</div></div></div>` : "";
+	}
 }
 
 new SettingsManager();
