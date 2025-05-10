f0edb0e17a4737b53980baf2b4f16647c637cfa6
deserialization <73188579+d15n@users.noreply.github.com>
2025-05-10 15:31:50 +0100

    format

--- src/frontend/modules/showPing.js
+++ src/frontend/modules/showPing.js
@@ -1,36 +1,35 @@
 class ShowPing {
-  constructor() {
-    this.originalGenList = window.windows[22].genList;
+	constructor() {
+		this.originalGenList = window.windows[22].genList;
 
-    window.glorpClient.settings.toggleShowPing = (enabled) =>
-      this.toggle(enabled);
+		window.glorpClient.settings.toggleShowPing = (enabled) => this.toggle(enabled);
 
-    this.toggle(true);
-  }
+		this.toggle(true);
+	}
 
-  toggle(enabled) {
-    if (enabled) {
-      window.windows[22].genList = this.modifiedGenList.bind(this);
-    } else window.windows[22].genList = this.originalGenList;
-  }
+	toggle(enabled) {
+		if (enabled) {
+			window.windows[22].genList = this.modifiedGenList.bind(this);
+		} else window.windows[22].genList = this.originalGenList;
+	}
 
-  modifiedGenList() {
-    const htmlString = this.originalGenList.call(this);
+	modifiedGenList() {
+		const htmlString = this.originalGenList.call(this);
 
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlString, "text/html");
-    const pingIcons = doc.querySelectorAll(".pListPing.material-icons");
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(htmlString, "text/html");
+		const pingIcons = doc.querySelectorAll(".pListPing.material-icons");
 
-    for (const icon of pingIcons) {
-      const pingValue = icon.getAttribute("title");
+		for (const icon of pingIcons) {
+			const pingValue = icon.getAttribute("title");
 
-      icon.classList.remove("pListPing", "material-icons");
-      icon.removeAttribute("title");
+			icon.classList.remove("pListPing", "material-icons");
+			icon.removeAttribute("title");
 
-      icon.textContent = `${pingValue ? pingValue : "N/A"} `;
-    }
-    return doc.body.innerHTML;
-  }
+			icon.textContent = `${pingValue ? pingValue : "N/A"} `;
+		}
+		return doc.body.innerHTML;
+	}
 }
 
 new ShowPing();
