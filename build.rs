54eee7d38716059799d8df8e10f1fce26fb52a88
slav <slaveetm@proton.me>
2026-03-04 20:10:39 +0200

    automatically change installer_script.wxs to the correct version in the build script deps

--- build.rs
+++ build.rs
@@ -11,9 +11,28 @@ fn main() {
 
     let toml_content = fs::read_to_string("Cargo.toml").unwrap();
     let toml: toml::Value = toml::from_str(&toml_content).unwrap();
+    let package_version = toml["package"]["version"].as_str().unwrap();
     let js_bundle_version = toml["package"]["metadata"]["js_bundle_version"].as_str().unwrap();
 
     let dest_path = env::current_dir().unwrap().join("target/bundle_version");
     fs::write(dest_path, js_bundle_version).unwrap();
-    println!("cargo:rerun-if-changed=cargo.toml");
+
+    let wxs_path = "resources/installer_script.wxs";
+    let wxs_content = fs::read_to_string(wxs_path).unwrap();
+    let regex = regex::Regex::new(r#"Version="([^"]+)""#).unwrap();
+
+    let current_version = regex
+        .captures(&wxs_content)
+        .and_then(|cap| cap.get(1))
+        .map(|m| m.as_str());
+
+    if current_version != Some(package_version) {
+        let updated_wxs = regex
+            .replace(&wxs_content, format!(r#"Version="{}""#, package_version))
+            .to_string();
+        fs::write(wxs_path, updated_wxs).unwrap();
+    }
+
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:rerun-if-changed=resources/installer_script.wxs");
 }
